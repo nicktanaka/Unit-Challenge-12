cd9fc13bea040de8857b4a4eb4017399ced03f80
Nicholas  Tanaka <nt40175@srcs.loc>
2017-03-01 08:05:50 -0800

    UC12 start

---
+++ functions.js
@@ -0,0 +1,61 @@
+/**
+* Removes part of string between two sub strings
+* @param {string} text The original string
+* @param {string} start The starting string
+* @param {string} end The ending string
+* @return {string} The string in between
+* @throws (Error} If start or end not found
+*/
+function between(string, start, end) {
+    var startAt = string.indexOf(start);
+
+    if (startAt == -1) {
+        throw new Error("No start found: " + start);
+    }
+
+    startAt += start.length;
+    var endAt = string.indexOf(end, startAt);
+
+    if (endAt == -1) {
+        throw new Error("No end found: " + end);
+    }
+
+    return string.slice(startAt, endAt);
+}
+
+/**
+ * Returns an area code from a phone number
+ * @param   {string} phoneNum The phone number
+ * @returns {string} The area code
+ */
+function getAreaCode(phoneNum) {
+
+    var areaCode;
+
+    try {
+        areaCode = between(phoneNum, "(", ")");
+    } catch (error) {
+        console.log(error.message);
+        return undefined;
+    }
+}
+/**
+ * Displays the area code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayAreaCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var areaCode = getAreaCode(phoneNum);
+        outputText = "Your area code is " + areaCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
+}
