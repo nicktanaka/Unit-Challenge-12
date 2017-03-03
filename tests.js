5caeda9e739a01ddf6ef7d98fa4cf78bc15a4380
Nicholas  Tanaka <nt40175@srcs.loc>
2017-03-03 09:03:46 -0800

    completed all tests

--- tests.js
+++ tests.js
@@ -3,3 +3,15 @@ QUnit.test("Test the getAreaCode function.", function (assert) {
     var result = getAreaCode(num);
     assert.deepEqual(result, "415", "Valid area code test passed.");
 });
+
+QUnit.test("Test the getCOCode function.", function (assert) {
+    var num = "(415) 261-8554";
+    var result = getCOCode(num);
+    assert.deepEqual(result, " 261", "Valid CO code test passed.");
+});
+
+QUnit.test("Test the getLineCode function.", function (assert) {
+    var num = "(415) 261-8554";
+    var result = getLineCode(num);
+    assert.deepEqual(result, "8554", "Valid line code test passed.");
+});
